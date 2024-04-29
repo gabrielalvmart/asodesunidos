2c11158c7efd7a7293e61dcab32306aecee199ec
Val14zp <valery14zp@gmail.com>
2024-04-29 04:01:09 -0600

    Merge resuelto

--- app/src/main/java/com/una/proyecto_moviles_asodesunidos/AsodesunidosDB.kt
+++ app/src/main/java/com/una/proyecto_moviles_asodesunidos/AsodesunidosDB.kt
@@ -1,5 +1,6 @@
 package com.una.proyecto_moviles_asodesunidos
 
+import android.util.Log
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -7,6 +8,7 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.getValue
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.una.models.LoanModel
 import com.una.models.UserModel
 
 object AsodesunidosDB {
@@ -70,6 +72,66 @@ object AsodesunidosDB {
             }
         })
     }
+    fun updateAssociate(updatedUser: UserModel, onComplete: (Boolean, String?) -> Unit) {
+        val userId = updatedUser.id
+        if (userId != "") {
+            val userRef = associatesRef.child(userId)
+            userRef.setValue(updatedUser)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        onComplete(true, null)
+                    } else {
+                        onComplete(false, task.exception?.message)
+                    }
+                }
+        } else {
+            onComplete(false, "El ID del usuario no puede estar vacío")
+        }
+    }
+
+    fun updateSavingsValue(userId: String, savingsType: String, newValue: Int, onComplete: (Boolean, String?) -> Unit) {
+        val userRef = associatesRef.child(userId)
+        userRef.child("savings").child(savingsType).setValue(newValue)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    onComplete(true, null)
+                } else {
+                    onComplete(false, task.exception?.message)
+                }
+            }
+    }
+    fun addLoanToAssociate(associateId: String, loan: LoanModel, onComplete: (Boolean, String?) -> Unit) {
+        getAssociate(associateId) { associate ->
+            if (associate != null) {
+                Log.d("MRG", associate.id)
+                Log.d("MRG", loan.amount.toString())
+                val loanID = generateLoanID()
+                loan.addID(loanID)
+                Log.d("MRG", loan.loanID!!)
+                associate.id = associateId
+
+                associate.addLoan(loan)
+                associatesRef.child(associateId).setValue(associate)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            onComplete(true, null)
+                            Log.d("MRG", "Correctly added")
+                        } else {
+                            onComplete(false, task.exception?.message)
+                            Log.e("MRG", "Not added at all")
+                        }
+                    }
+
+            } else {
+                onComplete(false, "Associate not found")
+            }
+        }
+    }
+
+    private fun generateLoanID(): String {
+        return "LOAN_" + System.currentTimeMillis().toString()
+    }
+
 
 
 }
